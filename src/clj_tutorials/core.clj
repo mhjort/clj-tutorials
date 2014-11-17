6e18b46db50132131e9807386f442798a1f6b718
Markus Hjort <markus.hjort@reaktor.fi>
2014-11-17 20:31:29 +0200

    Change core.async implementation to support five first steps

--- src/clj_tutorials/core.clj
+++ src/clj_tutorials/core.clj
@@ -1,12 +1,18 @@
 (ns clj-tutorials.core
-  (:require [clojure.core.async :as async :refer [go <! >!]]))
+  (:require [org.httpkit.client :as http]
+            [clojure.core.async :as async :refer [go <! >!]]))
 
+
+
+;STEP 1
 (defn run1 [number-of-users step]
   (let [cs (repeatedly number-of-users async/chan)]
     (doseq [c cs]
       (go (>! c (step))))))
 
 
+
+;STEP 2
 (defn- collect-result [cs]
   (let [[result c] (async/alts!! cs)]
     result))
@@ -18,64 +24,49 @@
     (repeatedly number-of-users #(collect-result cs))))
 
 
-
-(defn run-with-close-and-results [number-of-users step]
-  (let [cs (repeatedly number-of-users async/chan)]
-    (doseq [c cs]
-      (go (>! c (step))))
-    (let [results (repeatedly number-of-users #(collect-result cs))]
-      (doseq [c cs]
-        (async/close! c))
-      results)))
-
-
-
-(defn run-with-close-and-results2 [number-of-users step]
-  (let [cs (repeatedly number-of-users async/chan)]
-    (doseq [c cs]
-      (go (>! c (step))))
-    (let [results (repeatedly number-of-users #(collect-result cs))]
-      (dorun results)
-      (doseq [c cs]
-        (async/close! c))
-      results)))
-
-
-(defmacro with-channels [binding & body]
-  `(let [~(first binding) (repeatedly ~(second binding) async/chan)
-    ~'result (do ~@body)]
-    (dorun ~'result) ;Lazy results must be evaluated before channels are closed
-    (doseq [~'c ~(first binding)] (async/close! ~'c))
-  ~'result))
-
-(defn run-with-macro [number-of-users step]
-  (with-channels [cs number-of-users]
-    (doseq [c cs]
-      (go (>! c (step))))
-    (repeatedly number-of-users #(collect-result cs))))
-
+;STEP 3
 (defn bench [function]
   (let [start (System/currentTimeMillis)
         result (function)]
     [(- (System/currentTimeMillis) start) result]))
 
 (defn run-with-bench [number-of-users step]
-  (with-channels [cs number-of-users]
+  (let [cs (repeatedly number-of-users async/chan)]
     (doseq [c cs]
       (go (>! c (bench step))))
     (repeatedly number-of-users #(collect-result cs))))
 
 
 
+;STEP 4
+(defn http-get [url]
+  (fn []
+    (= 200 (:status @(http/get url)))))
+
+(defn run-with-url [number-of-users url]
+  (let [cs (repeatedly number-of-users async/chan)]
+    (doseq [c cs]
+      (go (>! c (bench (http-get url)))))
+    (repeatedly number-of-users #(collect-result cs))))
+
+
+
+
+;(STEP 5 "Non blocking http")
+
+(defn non-blocking-http-get [url]
+  (fn [cb]
+    (http/get url {} #(cb (= 200 (:status %))))))
+
 (defn callback->chan [fn-with-cb]
-  (let [start (System/currentTimeMillis)
-        c (async/chan)]
+  (let [c (async/chan)
+        start (System/currentTimeMillis)]
     (fn-with-cb #(async/put! c [(- (System/currentTimeMillis) start) %]))
     c))
 
-(defn run-non-blocking [number-of-users step]
+(defn run-non-blocking [number-of-users url]
   (let [cs (repeatedly number-of-users async/chan)]
     (doseq [c cs]
-      (go (>! c (callback->chan step))))
-    (let [results (repeatedly number-of-users #(collect-result cs))]
-      (repeatedly number-of-users  #(collect-result results)))))
+      (go (>! c (callback->chan (non-blocking-http-get url)))))
+    (let [results (mapv (fn [_] (collect-result cs)) cs)]
+      (doall (repeatedly number-of-users  #(collect-result results))))))
