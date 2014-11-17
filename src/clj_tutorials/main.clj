5f5a73eb6145090410729f27c6a7564711d1cc19
Markus Hjort <markus.hjort@reaktor.fi>
2014-11-17 20:30:25 +0200

    Add ongoing requests counter to test server

--- src/clj_tutorials/main.clj
+++ src/clj_tutorials/main.clj
@@ -3,8 +3,19 @@
             [compojure.handler :as handler]
             [org.httpkit.server :refer [run-server]]))
 
+(def ongoing-requests (atom 0))
+
+(defn- pong []
+  (let [ongoing-reqs (swap! ongoing-requests inc)
+        start (System/currentTimeMillis)]
+    (when (= 0 (mod ongoing-reqs 10))
+      (prn "Ongoing requests " ongoing-reqs))
+    (Thread/sleep 50)
+    (swap! ongoing-requests #(- % 1))
+    "pong"))
+
 (defroutes app-routes
-  (GET "/ping" [] "pong"))
+  (GET "/ping" [] (pong)))
 
 (defn run []
   (run-server (handler/api app-routes) {:port 8080 :join? false}))
