0d1089f1a20fd698ea5889475b77ca2d6852ecbc
Markus Hjort <markus.hjort@reaktor.fi>
2014-11-24 21:27:10 +0200

    Use correct version of http-kit

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [http-kit "2.1.19"]
+                 [http-kit "2.1.16"]
                  [ring/ring-core "1.3.1"]
                  [javax.servlet/servlet-api "2.5"]
                  [compojure "1.2.1"]
