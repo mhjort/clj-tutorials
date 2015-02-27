392d26b893d92041df1f957e9354fd003e852e79
Markus Hjort <markus.hjort@reaktor.fi>
2015-02-27 22:36:05 +0200

    Add core.async to Clojurescript example

--- src/cljs/app.cljs
+++ src/cljs/app.cljs
@@ -1,16 +1,25 @@
 (ns app
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [ajax.core :refer [GET]]
             [domina :refer [by-id set-text!]]
             [domina.css :refer [sel]]
             [domina.events :refer [listen!]]
+            [cljs.core.async :as async :refer [<! >!]]
             [lively.core :as lively]))
 
-(lively/start "/js/app.js" {:on-reload #(.log js/console "Reloaded!")})
+;(lively/start "/js/app.js" {:on-reload #(.log js/console "Reloaded!")})
+
+(def c (async/chan))
 
 (defn set-result! [result]
   (set-text! (by-id "result") result))
 
 (defn ping []
-  (GET "http://localhost:8080/ping" {:handler #(set-result! %)}))
+  (GET "http://localhost:8080/ping" {:handler #(go (>! c %))}))
 
 (listen! (sel "button") :click #(ping))
+
+(go
+  (while true
+    (let [result (<! c)]
+      (set-result! result))))
